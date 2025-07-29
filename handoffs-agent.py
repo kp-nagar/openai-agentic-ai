2b7fefa85e6ebfe7420ca1617c03739bd86ea924
Kumarpal Nagar <kumarpal.nagar@gmail.com>
2025-07-29 21:10:25 +0530

    refine code.

--- handoffs-agent.py
+++ handoffs-agent.py
@@ -4,9 +4,10 @@ from openai import AsyncOpenAI
 
 model="gemini-1.5-pro"
 google_api_key = "..."
-google_client = AsyncOpenAI(base_url="https://generativelanguage.googleapis.com/v1beta/openai/", api_key=google_api_key)
+google_base_url = "https://generativelanguage.googleapis.com/v1beta/openai/"
 
 
+google_client = AsyncOpenAI(base_url=google_base_url, api_key=google_api_key)
 gemini_model = OpenAIChatCompletionsModel(model=model, openai_client=google_client)
 
 
